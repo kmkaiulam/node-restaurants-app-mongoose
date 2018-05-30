9fb1271a9b6507e3db3a42dde76eda7bd8619054
Kenny Lam <kmlam@ucdavis.edu>
2018-05-30 15:27:01 -0700

    connect app to heroku and travis

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://kmlam:testing123@ds133920.mlab.com:33920/restaurant-app-db';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://kmlam:testing123>@ds133570.mlab.com:33570/test-mlab-db';
 exports.PORT = process.env.PORT || 8080;
